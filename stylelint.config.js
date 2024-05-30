d1d3fa6411c5c9d2d8e8d429dcb52b883544158b
may_ <lammayyin@gmail.com>
2024-05-30 16:47:27 +0200

    Fixed lint warnings and errors
    Fixed all lint warnings and errors

--- stylelint.config.js
+++ stylelint.config.js
@@ -129,4 +129,10 @@ module.exports = {
     "value-list-comma-space-before": "never",
     "value-list-max-empty-lines": 0,
   },
+  ignoreFiles: [
+    '**/*.js',
+    '**/*.jsx',
+    '**/*.ts',
+    '**/*.tsx'
+  ],
 }
